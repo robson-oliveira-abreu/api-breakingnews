321f19b5aa33b877cf3f696889b6d8f3af9f0819
Robson Oliveira <robsonsilva.abreu@outlook.com>
2023-01-03 23:51:37 -0300

    create controller

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const app = express();
 const userRoute = require('./src/routes/user.route')
 
-const app = express();
+const port = 8080;
 
-app.use('/soma', userRoute)
+app.use(express.json());
 
-app.listen(8080, () => {
-    console.log('Server is running on port 8080');
-})
+app.use('/user', userRoute)
+
+app.listen(8080, () => console.log(`Server is running on port ${port}`))
